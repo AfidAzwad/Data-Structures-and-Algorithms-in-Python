e3f43ea536d0fe6e1b3c4e4fd410d46b5e843155
Azwad22 <mdazwad22@gmail.com>
2021-09-22 13:14:13 +0600

    binary search

---
+++ Binary Search/binarysearch..py
@@ -0,0 +1,24 @@
+# The query can be absent in this list
+# The array/list can be empty
+# The query can be repeated in the list
+
+def locate_cards(cards, query):
+    length = len(cards)
+    cards.sort(reverse=True)
+    position = 0
+
+    if length == 0:
+        print("There is no card in this list!!")
+
+    else:
+        for i in cards:
+            if i == query:
+                position = cards.index(query)
+                print(cards)
+                return position
+        return str(query + " is not in this list")
+
+
+cards = [5, 8, 9, 3, 2, 4, 6, 1]
+query = input("query:")
+print("Position :", locate_cards(cards, query))
